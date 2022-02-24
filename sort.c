c43835671548e5a928915ed1aee91a388d95daff
Jay Park <88129753+JayPark-UTEP@users.noreply.github.com>
2022-02-24 16:12:49 -0700

    Add files via upload

---
+++ sort.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <time.h>
+
+void swap(int v[], size_t k)
+{
+    int temp;
+    temp = v[k];
+    v[k] = v[k+1];
+    v[k+1] = temp;
+}
+
+void sort(int v[], size_t n)
+{
+    size_t i, j;
+    for (i = 0; i < n; i+=1) {
+        for (j = i - 1; j >= 0 & v[j] > v[j+1]; j-=1) {
+	    swap(v,j);
+	}
+    }
+}
+
+int main(int argc, char **argv)
+{
+
+    int print;
+    size_t i, n;
+    int values[1000000] = {1, 5, 12, 02 ,52 ,56};
+
+    if (argc != 3) {
+        printf("Usage: %s <n> <pr> where <n> is the number of integers to sort and <pr> is 1 to print and 0 otherwise\n", argv[0]);
+	exit(1);
+    }
+    n = atoi(argv[1]);   
+    print = atoi(argv[2]);
+    srand(time(0));
+    for (i = 0; i < n; i++) {
+        values[i] = rand();
+        if (print)
+            printf("%d\n", values[i]);
+    }
+    sort(values, n);
+    if (print) {
+        printf("\nsorted:\n");
+        for (i = 0; i < n; i++) 
+            printf("%d\n", values[i]);
+    }
+    else 
+        printf("values[0]: %d\n", values[0]);
+    // printf("It complied");
+    return(0);
+}
